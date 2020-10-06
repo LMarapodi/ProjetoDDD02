e4a3fdedb5318e8208a416724e10e1dbdf6c3abd
LMarapodi <lucasmarapodi@gmail.com>
2020-10-06 11:36:33 -0300

    Projeto  DDD - versão 1.0

---
+++ Projeto.Infra.Data/Mappings/FornecedorMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Projeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Infra.Data.Mappings
+{
+    public class FornecedorMap : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.ToTable("Fornecedor");
+
+            builder.HasKey(f => f.IdFornecedor);
+
+            builder.Property(f => f.IdFornecedor).HasColumnName("IdFornecedor").IsRequired();
+
+            builder.Property(f => f.Nome).HasColumnName("Nome").IsRequired().HasMaxLength(150);
+
+            builder.Property(f => f.Cnpj).HasColumnName("Cnpj").IsRequired().HasMaxLength(25);
+
+        }
+    }
+}
---
+++ Projeto.Infra.Data/Mappings/ProdutoMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Projeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Infra.Data.Mappings
+{
+    public class ProdutoMap : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("Produto");
+
+            builder.HasKey(p => p.IdProduto);
+
+            builder.Property(p => p.IdProduto).HasColumnName("IdProduto").IsRequired();
+
+            builder.Property(p => p.Nome).HasColumnName("Nome").IsRequired().HasMaxLength(150);
+
+            builder.Property(p => p.Preco).HasColumnName("Preco").IsRequired().HasColumnType("decimal(18,2)");
+
+            builder.Property(p => p.Quantidade).HasColumnName("Quantidade").IsRequired();
+
+            builder.HasOne(p => p.fornecedor).WithMany(f => f.Produtos).HasForeignKey(p => p.IdFornecedor);
+        }
+    }
+}
